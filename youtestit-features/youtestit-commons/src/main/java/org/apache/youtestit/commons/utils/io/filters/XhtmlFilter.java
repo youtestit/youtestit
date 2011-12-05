bec1f92da62b6f976533d92e8906321fdec888b3
Patrick Guillerm <patrickguillerm@gmail.com>
2011-12-05 23:08:52 +0100

    import skeleton

---
+++ youtestit-features/youtestit-commons/src/main/java/org/apache/youtestit/commons/utils/io/filters/XhtmlFilter.java
@@ -0,0 +1,32 @@
+package org.apache.youtestit.commons.utils.io.filters;
+
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.Locale;
+
+
+/**
+ * The Class XhtmlFilter.
+ */
+public class XhtmlFilter implements FilenameFilter {
+
+	/** The Constant XHTML_TYPE. */
+	private static final String	XHTML_TYPE	= ".XHTML";
+
+
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public boolean accept(File folder, String name) {
+		boolean result = false;
+		if (name != null) {
+			final String cleanName = name.toUpperCase(Locale.US);
+			result = cleanName.endsWith(XHTML_TYPE);
+		}
+
+		return result;
+	}
+
+}
