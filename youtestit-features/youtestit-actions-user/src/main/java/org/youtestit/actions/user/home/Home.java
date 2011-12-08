24f732e0aaab1fef076f63638337d6b63add5a69
Patrick Guillerm <patrickguillerm@gmail.com>
2011-12-08 01:07:27 +0100

    add Youtestit design

---
+++ youtestit-features/youtestit-actions-user/src/main/java/org/youtestit/actions/user/home/Home.java
@@ -0,0 +1,43 @@
+package org.youtestit.actions.user.home;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.jboss.logging.Logger;
+import org.jboss.seam.security.Identity;
+
+@SessionScoped
+@Named
+public class Home implements Serializable {
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = -4746673041363469036L;
+
+    @Inject
+    private Logger log;
+
+    
+    
+    
+    @Inject
+    Identity identity;
+
+    public void select(final String test) {
+        log.info(">>>> test :");
+    }
+
+    
+    public String getTest() {   
+        log.info("<<<<< get TEST <<<<<<<");
+       
+        return "Hello";
+    }
+
+    public void setTest() {
+        log.info("<<<<< set TEST <<<<<<<");
+    }
+
+}
