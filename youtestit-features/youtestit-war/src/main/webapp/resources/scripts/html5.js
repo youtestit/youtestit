d25cf417c8cd5e4010fa064636a80e3a60d951cf
Patrick Guillerm <patrickguillerm@gmail.com>
2011-12-08 02:39:01 +0100

    add HTML5 Internet Explorer fix

---
+++ youtestit-features/youtestit-war/src/main/webapp/resources/scripts/html5.js
@@ -0,0 +1,8 @@
+document.createElement("header"); 
+document.createElement("footer"); 
+document.createElement("section"); 
+document.createElement("aside"); 
+document.createElement("nav");
+document.createElement("menu"); 
+document.createElement("article"); 
+document.createElement("figure"); 
